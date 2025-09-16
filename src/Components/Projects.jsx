aaddbd1e7ffa77de0c13367b35ea611523c85f6d
shihad-hasan07 <shihadhasan607255@gmail.com>
2025-09-16 14:04:53 +0600

    My projects section updated

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,3 +1,5 @@
+import { FaGithub, FaRegPaperPlane } from "react-icons/fa";
+
 const projects = [
     {
         title: "Foco",
@@ -24,7 +26,7 @@ const projects = [
 
 export default function Projects() {
     return (
-        <section className="py-12 px-6 bg-[#1e0e31] bg-cover  2xl:bg-no-repeat 2xl:bg-center bg-[url('/assets/bg1.png')] h-full"  id="projects">
+        <section className="py-12 px-6 bg-[#1e0e31] bg-cover  2xl:bg-no-repeat 2xl:bg-center bg-[url('/assets/bg1.png')] h-full" id="projects">
             <div className="max-w-5xl mx-auto text-center text-white">
                 <h2 className="text-4xl font-bold mb-6">My Projects</h2>
                 <p className="text-lg text-gray-400 mb-12">
@@ -45,15 +47,16 @@ export default function Projects() {
                             <div className="relative ">
                                 <img
                                     src={project.image} alt={project.title}
-                                    className="w-full h-48 object-cover pb-2 transition duration-200 group-hover:blur-xs group-hover:brightness-75" />
+                                    className="w-full h-48 object-cover pb-2 transition duration-200 hover:p-10 group-hover:blur-xs group-hover:brightness-75" />
 
                                 {/* Hover overlay button */}
                                 <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-500">
                                     <button>
                                         <a href={project.live}
                                             target="_blank"
-                                            className="bg-blue-600 text-white px-5 py-2 rounded-lg shadow-lg hover:bg-blue-700 transition" >
-                                            Visit Project
+                                            className="bg-blue-600 text-white px-5 py-2 rounded-lg shadow-lg hover:bg-blue-700 transition flex items-center gap-2" >
+                                            <p>Live site</p>
+                                            <FaRegPaperPlane />
                                         </a>
                                     </button>
                                 </div>
@@ -72,11 +75,11 @@ export default function Projects() {
                             </div>
 
                             {/* Card Content */}
-                            <div className="px-4 pb-5 bg-blue-950 text-white flex flex-col justify-between">
-                                <h3 className="text-lg font-semibold">{project.title}</h3>
-                                <p>{project.description}</p>
+                            <div className="px-4 pb-5 bg-blue-950 text-white">
+                                <h3 className="text-xl pt-4 font-semibold font-name">{project.title}</h3>
+                                <p className="font-underName my-3">{project.description}</p>
                                 <button className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
-                                   <a href={project.github}>Code</a>
+                                    <a href={project.github} className="flex items-center gap-2">Code <FaGithub /></a>
                                 </button>
                             </div>
                         </div>
